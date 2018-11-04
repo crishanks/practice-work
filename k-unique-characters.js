e2e47e498872e26553d3252fe35f02ed372eec06
github username <crishanks12@gmail.com>
2018-11-03 18:22:09 -0600

    finished practice

--- k-unique-characters.js
+++ k-unique-characters.js
@@ -1,27 +1,33 @@
 function kUniqueCharacters(str) {
   let longestSubstring = '';
-  const k = parseInt(str[0]);
+  const numberOfUniqueLetters = parseInt(str[0]);
   const restOfString = str.slice(1);
 
   let uniqueLettersAmount = 0;
-  let uniqueLetters = [];
   for (let i = 0; i < restOfString.length; i++) {
     const currentLetter = restOfString[i];
-    for (let j = 0; j < restOfString.length; j++) {
+    for (let j = i + 1; j < restOfString.length; j++) {
       const currentCheckLetter = restOfString[j];
-      const slicedString = restOfString.slice(i, j + 1);
-      if (!uniqueLetters.includes(currentCheckLetter)) {
-        uniqueLetters.push(currentCheckLetter);
-        uniqueLettersAmount += 1;
-      }
-      if (uniqueLettersAmount > k && slicedString.length > longestSubstring.length) {
+      const slicedString = restOfString.slice(i, j);
+      const passesCheck = rightAmountOfUniqueLetters(numberOfUniqueLetters, slicedString);
+
+      if (passesCheck && longestSubstring.length < slicedString.length) {
         longestSubstring = slicedString;
-        uniqueLetters = [];
-        uniqueLettersAmount = 0;
       }
     }
   }
   return longestSubstring
+}
+
+function rightAmountOfUniqueLetters(numberOfLetters, stringToCheck) {
+  const uniqueLetters = {};
+  for (let i = 0; i < stringToCheck.length; i++) {
+    const currentLetter = stringToCheck[i];
+    uniqueLetters[currentLetter] = true;
+  }
+
+  return Object.keys(uniqueLetters).length <= numberOfLetters;
+
 }
 
 kUniqueCharacters("2aabbcbbbadef");
