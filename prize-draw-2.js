3d23cec96a9be3109f9f4ea58d1fb86f01fd77fe
github username <crishanks12@gmail.com>
2018-12-09 16:18:48 -0700

    updated prize draw to acount for same values

--- prize-draw-2.js
+++ prize-draw-2.js
@@ -27,11 +27,11 @@ const createNamesAndValuesArray = (st, totalNameValuesArray, n) => {
   });
   
   const sortedNameAndValuesArray = namesAndValuesArray.sort((a,b) => {
-    if (b[1] === a[1]) {
-      return a[0] - b[0];
-    } else {
+     if (b[1] === a[1]) {
+       return a[0].charCodeAt() - b[0].charCodeAt();
+     } else {
       return b[1] - a[1];
-    }
+     }
   });
   console.log(sortedNameAndValuesArray);
   return sortedNameAndValuesArray[n - 1][0];
@@ -50,12 +50,5 @@ const rank = (st, we, n) => {
   return result;
 }
 
-rank("William,Naoh,Lily,Daniel,Michael,James,Matthew,Jacob,Alexander,Samantha,Liam,Benjamin,Emily,Avery,Isabella,Lyli,Ava,Aubrey,Robert,Olivai,Emma,Natalie,Noah", [2,4,3,4,5,6,2,5,5,3,6,5,4,3,1,5,2,6,4,3,3,4,1], 11); // "Daniel"
-
-//Create a initialNamesValuesArray by first creating a namesArray. give each name a number value by assigning each letter of the alphabet a number (ASCII?) and then adding the length of the name to it. 
-
-//Then, map over the we array and multiply each number in the initialNameValuesArray by it's corresponding weight in that array to get a totalNamesValuesArray.
-
-//Then, loop through the namesArray and the totalNamesValuesArray. Put each name and its corresponding value (values should be at the same indexes since we haven't sorted yet) into an array and push that array into a namesAndValuesArray. Sort the array by the values greatest to least.
-
-//Last, loop through the nested namesAndValuesArray. Return the name in the nth array in the namesAndValuesArray;
+rank("baa,aab,Sofia,Michael,Andrew,Lily,Benjamin", [2, 2, 1, 4, 3, 1, 2], 4);
+//'aab'
