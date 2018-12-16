7348316ba2433d1e04639601cc777edc21a7fe23
github username <crishanks12@gmail.com>
2018-12-15 18:17:01 -0700

    created consolidate_cart, apply_clearance, and checkout methods

--- green-grocer.rb
+++ green-grocer.rb
@@ -17,6 +17,53 @@ def consolidate_cart cart
       end
     end
     consolidated_cart
-  end
+end
   
   consolidate_cart cart
+
+#   def apply_coupons cart_before_coupons, coupons
+#     cart_after_coupons = {}
+#     cart_before_coupons.each do |item, attributes_hash|
+#       coupons.each do |coupon|
+#         if coupon[:item] == item
+#           cart_after_coupons[item] = attributes_hash
+#           cart_after_coupons[item][:count] -= coupon[:num]
+#           cart_after_coupons[`#{item} W/ COUPON`] = attributes_hash
+#           cart_after_coupons[`#{item} W/ COUPON`][:count] = attributes_hash[:count] - coupon[:num]
+#         else
+#           cart_after_coupons[item] = attributes_hash
+#         end
+#       end
+#     end
+#     cart_after_coupons
+#   end
+  
+#   apply_coupons ({"AVOCADO"=>{:price=>3.0, :clearance=>true, :count=>2}, "KALE"=>{:price=>3.0, :clearance=>false, :count=>1}}, {:item => "AVOCADO", :num => 2, :cost => 5.0})
+
+def apply_clearance cart
+    cart.each do |item, attributes|
+        if attributes[:clearance]
+            attributes[:price] = (attributes[:price] * 0.8).round(2)
+        end
+    end
+    cart
+end
+
+apply_clearance
+
+def checkout cart, coupons
+    total_cost = 0
+    consolidated_cart = consolidate_cart(cart)
+    consolidated_cart_after_coupons = apply_coupons(consolidated_cart, coupons)
+    apply_clearance(consolidated_cart_after_coupons)
+    consolidated_cart_after_coupons.each do |item, attributes|
+        total_cost += attributes[:price]
+    end
+    
+    if total_cost > 100
+        total_cost *= 0.9
+    end
+    total_cost
+end
+
+checkout
