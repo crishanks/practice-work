53374bba5c486806eda30afb20e80033011b6fe9
github username <crishanks12@gmail.com>
2018-12-17 19:16:36 -0700

    Big O notes on functions

--- are-they-the-same.js
+++ are-they-the-same.js
@@ -1,17 +1,18 @@
 function comp(array1, array2){
-    let powerArray = [];
-    for (let i = 0; i < array1.length; i++) {
-      powerArray.push(array1[i] * array1[i]);
-    }
-    if (array1 === undefined || array2 === undefined || powerArray.length !== array2.length) {
-      return false;
-    }
-    const sortedPowerArray = powerArray.sort().join('');
-    if (sortedPowerArray === array2.sort().join('')) {
-      return true;
-    }
+  let powerArray = [];
+  for (let i = 0; i < array1.length; i++) {
+    powerArray.push(array1[i] * array1[i]); //O(n)
+  }
+  if (array1 === undefined || array2 === undefined || powerArray.length !== array2.length) {
     return false;
   }
-  
-  //comp([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]);
-  //true;
+  const sortedPowerArray = powerArray.sort().join(''); // O(n log n) - performs a loop, and then a recursion to sort w/ quicksort
+  if (sortedPowerArray === array2.sort().join('')) //O(n log n) {
+    return true;
+  }
+  return false;
+}
+
+comp([121, 144, 19, 161, 19, 144, 19, 11], [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]);
+//true;
+//Function is O(n log n)
--- ascending-order.js
+++ ascending-order.js
@@ -1,5 +1,5 @@
 function isAscending(str) {
-    for (let i = 0; i < str.length - 1; i++) {
+    for (let i = 0; i < str.length - 1; i++) { //O(n)
       const currentLetter = str[i];
       const nextLetter = str[i + 1];
       if (nextLetter.charCodeAt(0) < currentLetter.charCodeAt(0)) {
@@ -8,12 +8,13 @@ function isAscending(str) {
     }
     return true;
   }
+  //function is O(n)
   
   function largestAscendingSubstring(str) {
     let result = '';
-    for (let i = 0; i < str.length; i++) {
+    for (let i = 0; i < str.length; i++) { //O(n)
       const slicedString = str.slice(i);
-      for (let j = 0; j < slicedString.length; j++) {
+      for (let j = 0; j < slicedString.length; j++) { //O(n^2) since nested loop will check each item in the outer array for each item in the inner array.
         const checkedSubstring = slicedString.slice(0, j + 1);
         const isInAscendingOrder = isAscending(checkedSubstring);
         if (isInAscendingOrder && checkedSubstring.length > result.length) {
@@ -23,6 +24,7 @@ function isAscending(str) {
     }
     return result;
   }
+  //function is O(n^2)
   
   largestAscendingSubstring('adealszjdf'); //'alsz'
   
