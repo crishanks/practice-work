32382b49e19d46649dbfdbb8b9ea5836e1833b87
github username <crishanks12@gmail.com>
2018-12-19 15:30:13 -0700

    cleaned up and optimized revrot function

--- reverse-or-rotate.js
+++ reverse-or-rotate.js
@@ -1,20 +1,33 @@
-function revrot(str, sz) {
-  let result = ""
-  for (let i = 0; i < str.length; i += sz) {
-    const substring = str.slice(i, i + sz);
-    if (substring.length >= sz) {
-      const substringArray = substring.split('');
-      const substringSum = substringArray.reduce(function (accumulator, currentValue) {
-        return accumulator + parseInt(currentValue);
-      }, 0);
-      if (Math.pow(substringSum, 3) % 2 === 0) {
-        result += substringArray.reverse().join('');
-      } else {
-        result += substring.slice(1) + substring[0];
-      }
+const revrot = (str, size) => {
+  if (size <= 0 || str === '' || size > str.length) {
+    return '';
+  }
+
+  let result = '';
+  for (let i = 0; i < str.length; i += size) {
+    const substring = str.slice(i, size + i);
+    const substringSum = substring.split('').reduce(function(accumulator, currentValue) {
+      return accumulator + Number(currentValue);
+    }, 0);
+    const sumCubedIsDivisibleBy2 = Math.pow(substringSum, 3) % 2 === 0;
+    if (substring.length < size) {
+      continue;
+    } else if (sumCubedIsDivisibleBy2) {
+      result += substring.split('').reverse().join('');
+    } else {
+      result += substring.slice(1) + substring[0];
     }
   }
   return result
 }
 
-revrot("123456987654", 6); // "234561876549"
+revrot("733049910872815764", 5); // '330479108928157'
+
+// The input is a string str of digits. Cut the string into chunks (a chunk here is a substring of the initial string) of size sz (ignore the last chunk if its size is less than sz).
+
+// If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+// If
+
+// sz is <= 0 or if str is empty return ""
+// sz is greater (>) than the length of str it is impossible to take a chunk of size sz hence return "".
