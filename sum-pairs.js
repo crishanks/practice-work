e39b301e821e9956905af58d1152744ba613b212
github username <crishanks12@gmail.com>
2018-12-19 17:31:39 -0700

    Optimized function from O(n^2) to O(n)

--- sum-pairs.js
+++ sum-pairs.js
@@ -1,44 +1,36 @@
-const sum_pairs = function(int_array, sum) {
-    let indexOfSecondSumNum = int_array.length;
-    let earliestPair = undefined;
-    for (let i = 0; i < int_array.length - 1; i++) {
-      const currentNum = int_array[i];
-      for (let j = i + 1; j < int_array.length; j++) {
-        const checkNum = int_array[j];
-        if (currentNum + checkNum === sum && j <= indexOfSecondSumNum) {
-          indexOfSecondSumNum = j;
-          earliestPair = [currentNum, checkNum];
-        } 
+function sumPairs(integerArray, sum) {
+    // O(n)
+    let foundNumbers = {};
+    for (let i = 0; i < integerArray.length; i++) {
+      const currentNum = integerArray[i];
+      const complementaryNumber = sum - currentNum;
+      if (foundNumbers[complementaryNumber]) {
+        return [complementaryNumber, currentNum];
+      } else {
+        foundNumbers[currentNum] = true;
       }
     }
-    return earliestPair;
   }
-  
-  sum_pairs([10,3,7,3,7,0], 10); //[3,7]
+  sumPairs([1,2,3,4,1,0], 2);//[1,1]
 
 
-//   function sum_pairs(int_array, sum) {
-//     let pairsObject = {};
-//     int_array.forEach(function(num) {
-//       const exists = pairsObject[num] !== undefined;
-//       if (!exists) {
-//         pairsObject[num] = true;
-//       }
-//     });
-//     console.log(pairsObject)
-  
-//     let result = undefined;
-//     for (let key in pairsObject) {
-//       if (pairsObject[sum - Number(key)] === true) {
-//         console.log([Number(key), sum - Number(key)])
-//         result = [Number(key), sum - Number(key)]
+
+// const sumPairs = function(int_array, sum) {
+//     let indexOfSecondSumNum = int_array.length;
+//     let earliestPair = undefined;
+//     for (let i = 0; i < int_array.length - 1; i++) {
+//       const currentNum = int_array[i];
+//       for (let j = i + 1; j < int_array.length; j++) {
+//         const checkNum = int_array[j];
+//         if (currentNum + checkNum === sum && j <= indexOfSecondSumNum) {
+//           indexOfSecondSumNum = j;
+//           earliestPair = [currentNum, checkNum];
+//         } 
 //       }
 //     }
-//     if (result !== undefined) {
-//       result.sort(function(a,b) {
-//         return int_array.indexOf(a) - int_array.indexOf(b);
-//       });
-//     }
-//     return result
+//     return earliestPair;
 //   }
-//   sum_pairs([0,2,0], 0);//
+  
+//   sumPairs([10,3,7,3,7,0], 10); //[3,7]
+
+
