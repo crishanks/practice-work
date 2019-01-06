e24410e472279e0da6cc67d8e28267593d15d756
github username <crishanks12@gmail.com>
2019-01-05 21:49:39 -0700

    strings, replace, forEach

---
+++ valid-parentheses.js
@@ -0,0 +1,11 @@
+function validParenthesis(str) {
+    str.split('').forEach((item) => {
+      str = str.replace('()', '');
+    });
+    if (str !== '') {
+      return false;
+    }
+    return true;
+  }
+  
+  validParenthesis("(())((()())())")
