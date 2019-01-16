062f39a59aad22efebd32b52f561a8e6a5eddc7d
github username <crishanks12@gmail.com>
2019-01-15 21:22:30 -0800

    revisited and optimized

--- longest-consecutive.js
+++ longest-consecutive.js
@@ -1,52 +1,19 @@
-function findLongestString(strarr) {
-    let currentLongest = '';
-    let currentLongestLength = 0;
-  
-    strarr.forEach((word) => {
-      if (word.length > currentLongestLength) {
-        currentLongest = word;
-        currentLongestLength = word.length;
-      }
-    });
-    return currentLongest;
+function longestConsec(strarr, k) {
+  let n = strarr.length; 
+  let str = '';
+
+  if (n = 0 || k > n || k <= 0) {
+    return str;
   }
-  
-  function longestConsec(strarr, k) {
-    if (strarr.length === 0 || k > strarr.length || k <= 0) {
-      return '';
+
+  for (let i = 0; i < strarr.length; i++) {
+    const currentString = strarr.slice(i, i + k).join('');
+    console.log(currentString);
+    if (currentString.length > str.length) {
+      str = currentString;
     }
-    const currentLongest = findLongestString(strarr);
-    let longestConsecutive = [];
-    let currentLongestConsecutive = 0;
-    for (let i = 1; i < strarr.length - 1; i++) {
-      const currentString = strarr[i];
-      const checkStringAfter = strarr[i + (k - 1)];
-      const checkStringBefore = strarr[i + (k - 3)];
-      if (currentLongest === currentString) {
-        // console.log('currentLongest: ', currentLongest);
-        // console.log("currentString: ", currentString);
-        // console.log('checkStringBefore: ', checkStringBefore);
-        // console.log('checkStringAfter: ', checkStringAfter);
-        if (currentString.length + checkStringBefore.length > currentString.length + checkStringAfter.length) {
-          if (currentString.length > checkStringBefore.length) {
-            currentLongestConsecutive = currentString.length + checkStringBefore.length;
-            longestConsecutive.push(currentString, checkStringBefore);
-            } else {
-              currentLongestConsecutive = currentString.length + checkStringBefore.length;
-              longestConsecutive.push(checkStringBefore, currentString);
-            }
-        } else if (currentString.length + checkStringAfter.length > currentString.length + checkStringBefore.length) {
-          if (currentString.length > checkStringAfter.length) {
-            currentLongestConsecutive = currentString.length + checkStringAfter.length;
-            longestConsecutive.push(currentString, checkStringAfter);
-          } else {
-            currentLongestConsecutive = currentString.length + checkStringBefore.length;
-            longestConsecutive.push(checkStringBefore, currentString);
-          }
-        }
-      }
-    }
-    return longestConsecutive.join('');
   }
-  
-  longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2); // "abigailtheta"
+  return str;
+}
+
+longestConsec([ 'hi', 'hello', 'hey','h' ], 2); // "hellohey"
