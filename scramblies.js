688498ecbd782833029ce0ab0666be0d4f350717
github username <crishanks12@gmail.com>
2019-01-23 17:54:41 -0800

    optimized from O(n^2) to O(n)

--- scramblies.js
+++ scramblies.js
@@ -1,83 +1,30 @@
-function createLetterCheck(str1, str2) {
-    let letters = {};
-    for (let i = 0; i < str2.length; i++) {
-      const exists = letters[str2[i]] !== undefined;
-      if (!exists) {
-        letters[str2[i]] = false;
-      }
+function scramble(str1, str2) {
+  let lettersNeeded = {};
+
+  for (let i = 0; i < str2.length; i++) {
+    const currentLetter = str2[i];
+    const letterExists = lettersNeeded[currentLetter] !== undefined;
+    if (!letterExists) {
+      lettersNeeded[currentLetter] = 1;
+    } else {
+      lettersNeeded[currentLetter] += 1;
     }
-    return letters;
   }
-  
-  function hasCorrectLetters(str1, str2) {
-    const letterObject = createLetterCheck(str1, str2);
-    for (let i = 0; i < str1.length; i++) {
-      const currentLetter = str1[i];
-      for (let j = 0; j < str2.length; j++) {
-        const currentCheckLetter = str2[j];
-        if (currentCheckLetter === currentLetter) {
-          letterObject[currentLetter] = true;
-        }
-      }
+
+  for (let i = 0; i < str1.length; i++) {
+    const currentLetter = str1[i];
+    const letterNeededExists = !!lettersNeeded[currentLetter];
+    if (letterNeededExists) {
+      lettersNeeded[currentLetter] -= 1;
     }
-  
-    for (let key in letterObject) {
-      if (letterObject[key] === false) {
-        return false;
-      }
-    }
-    return true;
   }
-  
-  function countLettersOne(str1, str2) {
-    let lettersCountOne = {};
-    for (let i = 0; i < str1.length; i++) {
-      const exists = lettersCountOne[str1[i]] !== undefined;
-      if (!exists) {
-        lettersCountOne[str1[i]] = 1;
-      } else  {
-        lettersCountOne[str1[i]] += 1;
-      }
+
+  for (let key in lettersNeeded) {
+    if (lettersNeeded[key] > 0) {
+      return false;
     }
-    return lettersCountOne;
   }
-  
-  function countLettersTwo(str1, str2) {
-    let lettersCountTwo = {};
-    for (let i = 0; i < str2.length; i++) {
-      const currentValue = str2[i];
-      const exists = lettersCountTwo[currentValue] !== undefined;
-      if (!exists) {
-        lettersCountTwo[currentValue] = 1;
-      } else  {
-        lettersCountTwo[currentValue] += 1;
-      }
-    }
-    return lettersCountTwo;
-  }
-  
-  function hasEnoughLetters(str1, str2) {
-    const lettersInOne = countLettersOne(str1, str2);
-    const lettersInTwo = countLettersTwo(str1, str2);
-    for (let key in lettersInTwo) {
-      for (let key2 in lettersInOne) {
-        if (key === key2) {
-          if (lettersInOne[key] < lettersInTwo[key]) {
-           return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-  
-  function scramble(str1, str2) {
-    const hasNeededLetters = hasEnoughLetters(str1, str2);
-    const hasRightLetters = hasCorrectLetters(str1, str2);
-    if(hasNeededLetters && hasRightLetters) {
-      return true;
-    }
-    return false;
-  }
-  
-  scramble("scriptingjava", "javaaaaaaaaaaaaascript"); //false;
+  return true;
+}
+
+scramble('rkqodlw','world'); //true
